17d8865afc971ba2cc48430de9b935b77005bcd8
Mikhail Yarmaliuk <hellboymxb@gmail.com>
2021-12-24 14:07:09 +0300

    chore: add more examples

--- example/index.ts
+++ example/index.ts
@@ -1,3 +1,4 @@
+import BaseException from '@core/base-exception';
 import Gateway from '@services/gateway';
 import Microservice from '@services/microservice';
 // import { Gateway, Microservice } from '../lib';
@@ -11,12 +12,22 @@ const microservice = Microservice.create({
 });
 
 microservice.addEndpoint('test', ({ hello }) => ({ success: true, hello }));
+microservice.addEndpoint('test-exception', () => {
+  throw new BaseException({
+    message: 'Oh noo...',
+    payload: {
+      field: 1,
+      field2: [],
+    },
+  });
+});
 
 /**
  * 1. Create gateway (auto registration microservices enabled)
  */
 const gateway = Gateway.create();
 
+// auto registration microservices enabled (no need call addMicroservice)
 // gateway.addMicroservice('demo');
 
 // start microservices
